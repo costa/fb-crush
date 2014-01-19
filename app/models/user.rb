ebe2d297fd247583ffbe57edfc870085e3334335
Costa Shapiro <costa@mouldwarp.com>
2014-01-19 22:25:03 +0200

    user model validation and optimisation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ActiveRecord::Base
   has_many :friends, :foreign_key => 'ego_id', :inverse_of => :ego
 
   validates_presence_of :name
+  validates_uniqueness_of :uid, :scope => :provider
+
 
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid']
-      if auth['info']
-         user.name = auth['info']['name'] || ""
-         user.email = auth['info']['email'] || ""
+      user.provider = auth[:provider]
+      user.uid = auth[:uid]
+      if info = auth[:info]
+         user.name = info[:name]
+         user.email = info[:email]
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140119085440) do
+ActiveRecord::Schema.define(version: 20140119172535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,5 +35,7 @@ ActiveRecord::Schema.define(version: 20140119085440) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
 
 end
