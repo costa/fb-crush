715eb9c3ff6a287f4fe97bbb5da48986359c6e13
Costa Shapiro <costa@mouldwarp.com>
2014-11-11 14:36:26 +0200

    delayed_job's ExceptionNotifier fix (for tests)

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -3,7 +3,7 @@ Delayed::Worker.class_eval do
 
     def handle_failed_job_with_notification(job, error)
       handle_failed_job_without_notification job, error
-      ExceptionNotifier.notify_exception error
+      ExceptionNotifier.notify_exception error  if defined? ExceptionNotifier
     end
     alias_method_chain :handle_failed_job, :notification
 
