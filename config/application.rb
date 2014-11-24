48bd2397d357dbedc91d289bb28ec61c745d8ada
Costa Shapiro <costa@mouldwarp.com>
2014-11-24 02:56:23 +0200

    a try to dix i18n-js vs heroku issue

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module FbCrush
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # XXX ?? https://github.com/fnando/i18n-js/issues/70
+    config.assets.initialize_on_precompile = true
   end
 end
