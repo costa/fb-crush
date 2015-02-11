85188c76a863002b2b968c424bafb34f9c228516
Costa Shapiro <costa@mouldwarp.com>
2015-02-11 16:52:48 +0200

    some styling and some branding

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,8 +1,8 @@
 if defined?(ExceptionNotification) && Rails.application.secrets.exception_notification_email.present?
   FbCrush::Application.config.middleware.use(ExceptionNotification::Rack,
     email: {
-      email_prefix: "FB-crush RTE: ",
-      sender_address: %{"FB-crush notifier" <notifier@#{Rails.application.secrets.domain_name}>},
+      email_prefix: "fb-CRUSH RTE: ",
+      sender_address: %{"fb-CRUSH notifier" <notifier@#{Rails.application.secrets.domain_name}>},
       exception_recipients: [Rails.application.secrets.exception_notification_email]
     }
   )
